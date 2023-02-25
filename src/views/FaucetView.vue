e2c752852bb9d444aa30ba8e02c5f9dae5a9144f
Ting-Jun Wang <levi900227@gmail.com>
2023-02-25 22:08:53 +0800

    feat: import bootstrap & add faucet page

---
+++ src/views/FaucetView.vue
@@ -0,0 +1,12 @@
+<template>
+  <button class="btn btn-primary">Click me!</button>
+</template>
+
+<script>
+import 'bootstrap/dist/css/bootstrap.css'; //导入Bootstrap样式表
+import 'bootstrap'; //导入Bootstrap JavaScript组件
+
+export default {
+  name: 'MyComponent',
+}
+</script>
