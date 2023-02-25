1ebb9668f7f06933ff8765de2b6090a4cd1514f1
Ting-Jun Wang <levi900227@gmail.com>
2023-02-25 22:51:46 +0800

    feat: use bootstrap template

--- src/views/FaucetView.vue
+++ src/views/FaucetView.vue
@@ -1,10 +1,24 @@
 <template>
-  <button class="btn btn-primary">Click me!</button>
+  <section class="page-section portfolio" id="portfolio">
+      <div class="container">
+          <!-- Portfolio Section Heading-->
+          <h2 class="page-section-heading text-center text-uppercase text-secondary mb-0">My Goerli Faucet</h2>
+          <!-- Icon Divider-->
+          <div class="divider-custom">
+              <div class="divider-custom-line"></div>
+              <div class="divider-custom-icon"><i class="fa-brands fa-ethereum"></i></div>
+              <div class="divider-custom-line"></div>
+          </div>
+
+          <!-- Portfolio Grid Items-->
+          <div class="row justify-content-center">
+              <button class='btn btn-primary'> Test </button>
+          </div>
+      </div>
+  </section>
 </template>
 
 <script>
-import 'bootstrap/dist/css/bootstrap.css'; //导入Bootstrap样式表
-import 'bootstrap'; //导入Bootstrap JavaScript组件
 
 export default {
   name: 'MyComponent',
